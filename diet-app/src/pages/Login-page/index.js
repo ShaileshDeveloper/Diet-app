8bf7a872e3aa2d5bac58a3c0e90ad28480fbb7bc
ShaileshDsce <74795973+ShaileshDeveloper@users.noreply.github.com>
2022-10-09 23:09:02 +0530

    finalised folder structure font-family

---
+++ diet-app/src/pages/Login-page/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function LoginPage() {
+  return (
+    <>
+    <div className='font-medium'>
+        LoginPage Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum, a ipsam ratione sapiente quas corporis itaque ab architecto! Fuga incidunt autem nostrum vel minus ratione aliquam a natus impedit voluptatem.
+    </div>
+    </>
+  )
+}
+
+export default LoginPage
